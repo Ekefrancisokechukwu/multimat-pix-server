ed72dee2463bc9724b7b872c79999a9e89d568c6
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-07-28 09:28:21 +0100

    feat: initialize backend with image upload functionality
    - Add package.json to manage dependencies and scripts
    - Implement image upload handling with multer and sharp for format conversion
    - Configure TypeScript with tsconfig.json

---
+++ src/app.ts
@@ -0,0 +1,79 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import fs from "fs";
+import multer from "multer";
+import path from "path";
+import { convertImageToFormats } from "./utils/sharpTransform";
+
+const app: Express = express();
+
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const dir = "./uploads"; // file upload base
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir); // if no such diretory  create one else ignore
+    cb(null, dir);
+  },
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    const base = path.basename(file.originalname, ext);
+    cb(null, `${base}-${Date.now()}${ext}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10MB in bytes
+  },
+});
+
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      return res.status(413).json({ error: "File size exceeds 10MB." });
+    }
+    next(err);
+  }
+);
+
+app.post("/upload", upload.single("image"), async (req, res) => {
+  if (!req.file) return res.status(400).json({ error: "No image uploaded." });
+
+  try {
+    const filePath = req.file.path;
+    const baseName = path.parse(req.file.filename).name;
+
+    const transformedPaths = await convertImageToFormats(filePath, baseName);
+
+    res.json({
+      original: `/uploads/${req.file.filename}`,
+      formats: transformedPaths,
+    });
+  } catch (err) {
+    console.error(err);
+
+    // if (err.code === "LIMIT_FILE_SIZE") {
+    //   return res.status(413).json({ error: "File size exceeds 10MB limit." });
+    // }
+
+    res.status(500).json({ error: "Failed to process image." });
+  }
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Welcome to the Image Multimat  API" });
+});
+
+export default app;
---
+++ src/utils/sharpTransform.ts
@@ -0,0 +1,39 @@
+import path from "path";
+import fs from "fs";
+import sharp from "sharp";
+
+const outputFormats = ["jpeg", "png", "webp", "avif"] as const;
+type Format = (typeof outputFormats)[number];
+
+type File = {
+  path: string;
+  sizeInBytes: number;
+};
+
+const BASE_URL = process.env.BASE_URL || "http://localhost:5000";
+
+export const convertImageToFormats = async (
+  inputPath: string,
+  filename: string
+): Promise<{ [key in Format]: File }> => {
+  const outputDir = path.join(__dirname, "../../uploads");
+  fs.mkdirSync(outputDir, { recursive: true });
+
+  const results: { [key in Format]?: File } = {};
+
+  await Promise.all(
+    outputFormats.map(async (format) => {
+      const outPath = path.join(outputDir, `${filename}-converted.${format}`);
+      await sharp(inputPath).toFormat(format).toFile(outPath);
+
+      const { size } = fs.statSync(outPath);
+
+      results[format] = {
+        path: `${BASE_URL}/uploads/${filename}-converted.${format}`,
+        sizeInBytes: size,
+      };
+    })
+  );
+
+  return results as { [key in Format]: File };
+};
